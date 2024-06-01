375522e498d7d8eff5a4491c82767e870378ed28
ustp-cc <162346459+ustp-cc@users.noreply.github.com>
2024-06-01 19:15:55 +0800

    commit-pit

---
+++ PIT_Cortes/PITJS_Cortes.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const target = this.getAttribute('href');
+            scrollToElement(target, getOffset(target));
+        });
+    });
+});
+
+function scrollToElement(selector, offset) {
+    const element = document.querySelector(selector);
+    if (element) {
+        const offsetTop = element.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+            top: offsetTop - offset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function getOffset(selector) {
+    switch (selector) {
+        case '#skills':
+            return 50;
+        case '#projects':
+            return 125;
+        default:
+            return 100;
+    }
+}
